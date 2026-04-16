52108a86de44cc40649d7b3218e8d837f74d2e7e
Krzysztof Urban <krzysztofurban.it@gmail.com>
2026-04-16 20:09:55 +0200

    Add JWT validation

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "anypoint_apim_flexgateway" "fg" {
   org_id                     = var.root_org
   env_id                     = var.env_id
   asset_group_id             = var.root_org
-  asset_id                   = "test-api"
+  asset_id                   = "example-api"
   asset_version              = "1.0.0"
   deployment_target_id       = data.anypoint_flexgateway_target.target.id
   deployment_target_name     = data.anypoint_flexgateway_target.target.name
@@ -64,4 +64,24 @@ resource "anypoint_apim_flexgateway" "fg" {
   }
 }
 
-
+resource "anypoint_apim_policy_jwt_validation" "jwtpolicy" {
+  org_id = var.root_org
+  env_id = var.env_id
+  apim_id = anypoint_apim_flexgateway.fg.id
+  disabled = false
+  asset_version = "0.11.1"
+  configuration_data {
+    jwt_origin = "httpBearerAuthenticationHeader"
+    signing_method = "rsa"
+    signing_key_length = 256
+    jwt_key_origin = "jwks"
+    jwks_url = "http://your-jwks-service.example:80/base/path"
+    jwks_service_time_to_live = 60
+    jwks_service_connection_timeout = 10000
+    claims_to_headers = []
+    skip_client_id_validation = false
+    text_key = "your-(256|384|512)-bit-secret"
+    client_id_expression = "#[vars.claimSet.client_id]"
+    validate_aud_claim = false
+  }
+}
